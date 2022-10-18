09e23d06ae5942a3f141b5b971f583626592e97e
salcasta <salcasta@gmail.com>
2022-10-18 16:43:14 -0500

    Added PREP

--- question002.js
+++ question002.js
@@ -8,14 +8,24 @@ Write a function that when given a URL as a string, parses out just the domain n
 * url = "https://www.cnet.com"                -> domain name = cnet"
 */
 
+// Write a function that when given a URL as a string, parses out just the domain name and returns it as a string. For example:
+
+// domainName("http://github.com/carbonfive/raygun") == "github" 
+// domainName("http://www.zombie-bites.com") == "zombie-bites"
+// domainName("https://www.cnet.com") == "cnet"
+
+//url trad no funny biz
+//str 
+
 function domainName(url) {
+    //replace the stuff upfront
     let abbrStr = url.replace('http://', '').replace('https://', '').replace('www.', '')
     
+    //split grabbing the first value
     let domain = abbrStr.split('.')[0]
     
     return domain
 }
-
 
 
 //CLEANED UP
