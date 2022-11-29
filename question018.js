885a41390adc06174c336df6cfb2bbb35e238bc8
salcasta <salcasta@gmail.com>
2022-11-29 17:30:16 -0600

    remove consecutive letters in a string when given an array with multiple strings. Using map,filter,split/join.

---
+++ question018.js
@@ -0,0 +1,19 @@
+//String array duplicates
+
+
+/*
+In this Kata, you will be given an array of strings and your task is to remove all consecutive duplicate letters from each string in the array.
+
+For example:
+
+dup(["abracadabra","allottee","assessee"]) = ["abracadabra","alote","asese"].
+
+dup(["kelless","keenness"]) = ["keles","kenes"].
+
+Strings will be lowercase only, no spaces. See test cases for more examples.
+*/
+
+function removeDupes(arr){
+    //map -> word - split - filter - join
+    return arr.map(word => word.split('').filter((l,i,a)=> l != a[i-1] ).join(''))
+}
